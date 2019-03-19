21f26e3f4edcda319c9b2ad0b78eb2a4660d342f
eric ho <wingsum.developer@gmail.com>
2019-03-19 23:21:51 +0800

    - remove two dummy activity page - add koin injection library

--- app/src/main/java/com/ericho/itunes_music/data/repository/MusicRepository.kt
+++ app/src/main/java/com/ericho/itunes_music/data/repository/MusicRepository.kt
@@ -2,10 +2,11 @@ package com.ericho.itunes_music.data.repository
 
 import com.ericho.itunes_music.data.datasource.MusicDataSource
 
-class MusicRepository(val remoteDataSource: MusicDataSource): MusicDataSource{
-
-    override fun getMusicList(searchString: String, callBack: MusicDataSource.LoadMusicCallback) {
-        remoteDataSource.getMusicList(searchString, callBack)
-    }
-
+/**
+ * Created by steve_000 on 19/3/2019.
+ * for project itune-music-mvvm
+ * package name com.ericho.itunes_music.data.repository
+ */
+interface MusicRepository {
+    fun getMusicList(searchString: String, callBack: MusicDataSource.LoadMusicCallback)
 }
