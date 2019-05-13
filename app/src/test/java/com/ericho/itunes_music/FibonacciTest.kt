8e887db3ca16471d7e5a476e18b43467cadcbbcd
eric ho <wingsum.developer@gmail.com>
2019-05-13 12:57:25 +0800

    update test case

---
+++ app/src/test/java/com/ericho/itunes_music/FibonacciTest.kt
@@ -0,0 +1,51 @@
+package com.ericho.itunes_music
+
+import org.junit.Test
+
+
+class FibonacciTest {
+
+    @Test
+    fun xxx() {
+
+        val testInput = arrayOf(1, 22, 9, 12, 14, 376, 378, 317812, 317811, 317810)
+            .toIntArray()
+        nextFibonacci(testInput)
+    }
+
+    fun nextFibonacci(input: IntArray) {
+        val max = input.max()!!
+        val fibonacciList = buildSequence(max).toList()
+
+        println("Output:")
+        for (value: Int in input) {
+            // find the next fibonacci value for each number in array
+            var a = 0
+            for ((index, fibonacciValue) in fibonacciList.withIndex()) {
+                val isNextFibonacciNumber = value < fibonacciValue
+                if (isNextFibonacciNumber) {
+                    a = index
+                    break
+                }
+            }
+            println(fibonacciList[a])
+        }
+
+    }
+
+    private fun buildSequence(number: Int): Sequence<Int> {
+        return sequence {
+            var terms = Pair(1, 1)
+
+            while (true) {
+                yield(terms.first)
+                terms = Pair(terms.second, terms.first + terms.second)
+
+                if (terms.first > number) {
+                    yield(terms.first)
+                    break
+                }
+            }
+        }
+    }
+}
